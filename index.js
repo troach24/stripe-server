91e70d2a6cbf4a2954e10c8c411e0c3cd254edbe
Travis Roach <travis.roach24@gmail.com>
2018-06-07 16:06:29 -0600

    committing complete de-coupled server

---
+++ index.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World! 🌈'
+  });
+});
+
+app.post('/accept-payment', (req, res) => {
+  const token = req.body.stripeToken;
+  const amount = req.body.amount;
+  console.log(req.body);
+  
+  const charge = stripe.charges.create({
+    amount,
+    currency: 'usd',
+    description: 'All the things',
+    source: token,
+  });
+
+  charge.then(result => {
+    res.json(result);
+  })
+    .catch(err => {
+      const error = {
+        message: err.message,
+        failure_code: err.code || 500
+      };
+      res.json(error);
+    });
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  const error = new Error('Not Found. 🔍');
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message,
+    error: error.stack
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
